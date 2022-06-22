330d18256bc4049356c4fc5d71fd8e229d03b769
raitasu <pavelburyak3@gmail.com>
2022-06-22 18:19:21 +0300

    Updates

---
+++ precache-manifest.f08b9b9f430b22a01be6ba9729575455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297029784c8a535d052014f2046fcae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d578a81764a74a044835",
+    "url": "/static/css/main.96a7194e.chunk.css"
+  },
+  {
+    "revision": "fbb1dc010a04add9bb54",
+    "url": "/static/js/2.a535d0f4.chunk.js"
+  },
+  {
+    "revision": "a0afa9dd810a508dc405326e8bd36d1b",
+    "url": "/static/js/2.a535d0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d578a81764a74a044835",
+    "url": "/static/js/main.cf76d4cd.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
